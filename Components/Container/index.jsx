078cd8e5b883094db49e6fe62b3d55f8dc5897e5
marceloRamos <marcelotanaca@gmail.com>
2022-01-26 21:09:51 -0300

    nova estilização com styled-components

--- Components/Container/index.jsx
+++ Components/Container/index.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-
+import styled from "styled-components";
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import { fundoClaro } from "../UI/Variaveis";
 
-const Container = () => {
+const Container = styled.div `
+  background-color: ${fundoClaro};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const Conteudo = styled.section `
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </Container>
   );
 };
 
-export default Container;
+
